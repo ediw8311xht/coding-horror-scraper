c7e92c56bc2165dd82db21c73fd68f0e66c44c25
Unknown <maxballard333@gmail.com>
2017-03-29 23:19:34 -0400

    first commit (basic)
    added main.py
    added articleScraper.py
    added .gitignore
    
    added stuff to .gitignore so that it ignore articles.txt and .pyc files
    
    added functioins in articleScraper:
    -getArticles(page) that returns  articles(unformated) from a certain page.
    -formatArticle(article) formats article as txt file then returns it.
    -getNumPages() gets number of pages of coding horror website.
    
    main.py:
    -readFile(File): reads file then returns it as a string.

---
+++ articleScraper.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import requests
+
+def getArticles(page):
+    articles = []
+    url = "https://blog.codinghorror.com/page/%i/" % (page)
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    for i in soup.find_all("article", class_="post"):
+        articles.append(i)
+    return articles
+
+def formatArticle(article):
+    start = 0
+    for i in range(0, len(article)):
+        start = i
+        if article[i] != " " or article[i] != "\n":
+            break
+    tArticle = article.get_text()[0:start]
+    Time = soup.find(attrs={"time": "datetime"})
+    print Time
+    return tArticle
+
+def getNumPages():
+    url = "https://blog.codinghorror.com/page/2/"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    pageNum = soup.find("span", class_="page-number").getText()
+    return int(pageNum[10:len(pageNum)])
---
+++ main.py
@@ -0,0 +1,21 @@
+import os.path
+import articleScraper
+
+def readFile(File):
+    String = ""
+    with open(File, 'r') as File:
+        String = File.read()
+    return String
+
+if __name__ == "__main__":
+    fn = "articles.txt"
+    try:
+        a = readFile(fn)
+    except IOError:
+        File = open(fn, 'w')
+        File.write("0\n\n")
+        File.close()
+        a = readFile(fn)
+    numPages = articleScraper.getNumPages()
+    less = numPages - int(a[0:10].strip())
+    print less
